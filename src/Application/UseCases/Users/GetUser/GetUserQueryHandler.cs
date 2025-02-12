70a0093c5dd0e1eb4aed2e61cf39eb0612ecf438
Vinicius Oliveira <eovinicius10@gmail.com>
2025-02-12 17:52:46 -0300

    update

--- src/Application/UseCases/Users/GetUser/GetUserQueryHandler.cs
+++ src/Application/UseCases/Users/GetUser/GetUserQueryHandler.cs
@@ -1,36 +1,36 @@
-﻿using System.Data.Common;
+﻿// using System.Data.Common;
 
-using Application.Messaging;
+// using Application.Messaging;
 
-using SharedKernel.Erros;
+// using SharedKernel.Erros;
 
-namespace Application.UseCases.Users.GetUser;
+// namespace Application.UseCases.Users.GetUser;
 
-internal sealed class GetUserQueryHandler(IDbConnectionFactory dbConnectionFactory)
-    : IQueryHandler<GetUserQuery, UserResponse>
-{
-    public async Task<Result<UserResponse>> Handle(GetUserQuery request, CancellationToken cancellationToken)
-    {
-        await using DbConnection connection = await dbConnectionFactory.OpenConnectionAsync();
+// internal sealed class GetUserQueryHandler(IDbConnectionFactory dbConnectionFactory)
+//     : IQueryHandler<GetUserQuery, UserResponse>
+// {
+//     public async Task<Result<UserResponse>> Handle(GetUserQuery request, CancellationToken cancellationToken)
+//     {
+//         await using DbConnection connection = await dbConnectionFactory.OpenConnectionAsync();
 
-        const string sql =
-            $"""
-             SELECT
-                 id AS {nameof(UserResponse.Id)},
-                 email AS {nameof(UserResponse.Email)},
-                 first_name AS {nameof(UserResponse.FirstName)},
-                 last_name AS {nameof(UserResponse.LastName)}
-             FROM users.users
-             WHERE id = @UserId
-             """;
+//         const string sql =
+//             $"""
+//              SELECT
+//                  id AS {nameof(UserResponse.Id)},
+//                  email AS {nameof(UserResponse.Email)},
+//                  first_name AS {nameof(UserResponse.FirstName)},
+//                  last_name AS {nameof(UserResponse.LastName)}
+//              FROM users.users
+//              WHERE id = @UserId
+//              """;
 
-        UserResponse? user = await connection.QuerySingleOrDefaultAsync<UserResponse>(sql, request);
+//         UserResponse? user = await connection.QuerySingleOrDefaultAsync<UserResponse>(sql, request);
 
-        if (user is null)
-        {
-            return Result.Failure<UserResponse>(UserErrors.NotFound(request.UserId));
-        }
+//         if (user is null)
+//         {
+//             return Result.Failure<UserResponse>(UserErrors.NotFound(request.UserId));
+//         }
 
-        return user;
-    }
-}
+//         return user;
+//     }
+// }
